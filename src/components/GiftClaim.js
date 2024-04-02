72894403a2e50ebb652cea054496a5af188d9a27
ThanhTuan1695 <tuan.it.1695@gmail.com>
2024-04-02 23:26:17 +0700

    list and claim gift

--- src/components/GiftClaim.js
+++ src/components/GiftClaim.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect }  from "react";
-import { useChainId, useAccount, useReadContract } from "wagmi";
+import { useChainId, useAccount, useReadContract, useWriteContract, useSwitchChain,useWaitForTransactionReceipt  } from "wagmi";
 import XGift_ABI from "../config/XGift_ABI.json";
 import chainPolymer from "../config/chainPolymer.json";
-import {BrowserProvider, Contract} from 'ethers';
+import { Contract, JsonRpcProvider, formatUnits, encodeBytes32String,  } from 'ethers';
 import { toast} from 'react-toastify';
 
 
@@ -10,43 +10,80 @@ function GiftClaim  ()  {
     const [listGift, setListGift] = useState([]);
     const {address} = useAccount();
     const chainId = useChainId();
+    const { switchChain } = useSwitchChain()
+    const { data: hash, error, writeContract } =  useWriteContract();
+    const { isLoading: isConfirming, isSuccess: isConfirmed } =
+      useWaitForTransactionReceipt({
+        hash,
+      });
 
-    const { data: contractData, error, isLoading } = useReadContract({
+
+    const { data, isError } = useReadContract({
         chainId: chainId,
         address: chainPolymer.optimism.portAddr,
         abi: XGift_ABI,
         functionName: 'getGiftsByUser',
         args: [address]
     });
-    console.log({
-        chainId: chainId,
-        address: chainPolymer.optimism.portAddr,
-        functionName: 'getGiftsByUser',
-        args: [address]
-    });
-    console.log(contractData);
     useEffect(() => {
+
+        if (isConfirming && !isConfirmed) {
+            toast.info("Waiting for confirmation...");
+        }
+
+        if (isConfirmed) {
+            toast.success('Transaction confirmed!!!');
+            const explorerUrl = chainPolymer.optimism.explorerUrl + "/tx/" + hash;
+            toast.info(`Transaction explorer:  ${explorerUrl}`);
+        }
+        if (error){
+            toast.error(error.shortMessage);
+        }
+
+        if (isError){
+            toast.error(isError.shortMessage);
+        }
+
         const getGiftDetails = async () => {
-            if (contractData) {
-                const provider = new BrowserProvider(window.ethereum);
+            if (data) {
+                const provider = new JsonRpcProvider("https://sepolia.optimism.io");
                 const contract = new Contract(chainPolymer.optimism.portAddr, XGift_ABI, provider);
 
                 const gifts = [];
-                console.log(contractData);
-                for (const id of contractData) {
-                    console.log(id);
-                    const gift = await contract.gifts(id);
-                    gifts.push(gift);
+                for (const id of data) {
+                    const gift =  await contract.gifts(id);
+                    const floatingAmount = formatUnits(gift.amount, 18);
+                    const giftDetail = {
+                        "id" : gift.id,
+                        "sender": gift.sender,
+                        "amount" : floatingAmount,
+                        "isClaimed" : gift.isClaimed
+                    }
+                    gifts.push(giftDetail);
                 }
                 setListGift(gifts);
             }
         };
 
         getGiftDetails();
-    }, [contractData]);
-    const handleClaim = (byte32, index) => {
+
+    }, [data, isError, isConfirming, isConfirmed, error, hash]);
+
+    const handleClaim = async (giftId, index) => {
         // Xử lý logic khi nhấn nút Claim cho phần tử có index là index
-        console.log('Claim clicked for byte32:', byte32, 'at index:', index);
+        console.log('Claim clicked for byte32:', giftId, 'at index:', index);
+        if(chainId !== chainPolymer.optimism.channelId){
+            await switchChain({chainId: chainPolymer.optimism.channelId});
+        }
+        console.log({ 
+            args: [encodeBytes32String(chainPolymer.optimism.channelId), chainPolymer.optimism.timeout, giftId],
+          });
+        writeContract({
+            address: chainPolymer.optimism.portAddr,
+            abi: XGift_ABI,
+            functionName: 'claimGift',
+            args: [encodeBytes32String(chainPolymer.optimism.channelId), chainPolymer.optimism.timeout, giftId],
+          });
       };
     
 
@@ -56,26 +93,27 @@ function GiftClaim  ()  {
             <thead>
                 <tr>
                     <th scope="col">#</th>
-                    <th scope="col">Gift ID</th>
-                    <th scope="col">Sender</th>
-                    <th scope="col">Receiver</th>
+                    <th scope="col">ID</th>
                     <th scope="col">Amount</th>
                     <th scope="col">Claim Status</th>
                     <th scope="col">Claim Gift</th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody>       
                 {listGift.map((gift, index) => (
-                    <tr key={index}>
+                    <tr key={gift.id}>
                         <td>{index + 1}</td>
                         <td>{gift.id}</td>
-                        <td>{gift.sender}</td>
-                        <td>{gift.receiver}</td>
                         <td>{gift.amount}</td>
                         <td>{gift.isClaimed ? 'Claimed' : 'Not Claimed'}</td>
                         <td>
-                            {/* Nút Claim */}
-                            <button class="btn btn-secondary" onClick={() => handleClaim(gift.id, index)}>Claim</button>
+                            {gift.isClaimed ? 
+                              <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16" style={{color: "green"}}>
+                              <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
+                            </svg>
+                                :
+                              <button class="btn btn-secondary" onClick={() => handleClaim(gift.id, index)}>Claim</button>
+                            }
                         </td>
                     </tr>
                 ))}
